4f10ae422495c8bd01bb7e2a8792245036b53e23
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2025-07-14 22:52:10 -0300

    Atualização de arquivo

--- pos-graduacao/infra-código-terraform-aws-azure-databricks/05-terraform-fundamentos-parte-1/Lab2-Parte2/tarefa4/main.tf
+++ pos-graduacao/infra-código-terraform-aws-azure-databricks/05-terraform-fundamentos-parte-1/Lab2-Parte2/tarefa4/main.tf
@@ -5,5 +5,5 @@ module "dsa_ec2_instances" {
   instance_count = 2
   ami_id         = "ami-0a0d9cf81c479446a"
   instance_type  = "t2.micro"
-  subnet_id      = "subnet-01e1af37d8c9c9495"
+  subnet_id      = "subnet-09b8a72753e29c3bf"
 }
